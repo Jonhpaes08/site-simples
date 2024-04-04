066bb17bc74a76f9279d58845274043740416d3a
Jonhvmp <jonhpaz08@gmail.com>
2024-04-04 06:27:41 -0300

    Projeto Finalizado
    Site com alternância de temas entre 'Claro / Escuro' usando JavaScript.
    
    Curso - Site simples usando HTML, CSS e JavaScript - Fundação Bradesco | Microsoft

--- app.js
+++ app.js
@@ -3,15 +3,16 @@
 const switcher = document.querySelector('.btn'); /* 'document.querySelector' para obter a referência do botão */
 
 switcher.addEventListener('click', function() { /* Adicionando o ouvinte e o manipulador do evento 'click' */
-    document.body.classList,toggle('dark-theme')
+    document.body.classList.toggle('dark-theme')
 
-    var className = document.body.className;
+    var className = document.body.className; /* Adicionando uma instrução 'if' para verificar 
+                                                o tema atual e atualizar o rótulo do  botão*/                                         
     if(className == "light-theme"){
-        this.textContent = "Dark";
+        this.textContent = "Escuro";
     }
     else{
-        this.textContent = "light";
+        this.textContent = "Claro";
     }
 
+    console.log('current class name: ' + className);
 });
-
