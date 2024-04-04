2fe30cb20ae639c5c673415f59f1511ba453ec45
Jonhvmp <jonhpaz08@gmail.com>
2024-04-04 04:33:56 -0300

    Usando Js para alternar os temas entre Dark e Light

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 'use strict'
+
+const switcher = document.querySelector('.btn'); /* 'document.querySelector' para obter a referência do botão */
+
+switcher.addEventListener('click', function() { /* Adicionando o ouvinte e o manipulador do evento 'click' */
+    document.body.classList,toggle('dark-theme')
+
+    var className = document.body.className;
+    if(className == "light-theme"){
+        this.textContent = "Dark";
+    }
+    else{
+        this.textContent = "light";
+    }
+
+});
+
