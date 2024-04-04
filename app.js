bad03f3e5bfd306db917e7a02c75b853a4d05a6a
Jonhvmp <jonhpaz08@gmail.com>
2024-04-04 06:51:41 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const switcher = document.querySelector('.btn'); /* 'document.querySelector' para obter a referência do botão */
+const switcher = document.querySelector('.btn');  /* 'document.querySelector' para obter a referência do botão */
 
 switcher.addEventListener('click', function() { /* Adicionando o ouvinte e o manipulador do evento 'click' */
     document.body.classList.toggle('dark-theme')
